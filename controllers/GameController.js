9b25417c8940c14ac85a0fee647c752f1ee74620
Morgan Zolob <mzolob@gmail.com>
2016-07-28 18:17:36 -0700

    Add timeout before closing game connection
    Sometimes the game over window wouldn't come up, this will add a delay
    before closing the connection so the game over message should be sent.

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -248,11 +248,16 @@ module.exports = class GameController
 			// Send the game over message
 			pushUpdate({nextTurn: null, done: {black: black, white: white}});
 
-			// And close the connections
-			for (var ws of gameConnections[gameId]) {
-				if (ws)
-					ws.close(undefined, 'Game Over');
-			}
+			// Wait a second and then close the connections. We were having an issue where sometimes the connection would
+			// be closed before the pushUpdate finished
+			setTimeout(function() {
+				for (var ws of gameConnections[gameId]) {
+					if (ws)
+						ws.close(undefined, 'Game Over');
+				}
+			}, 2000)
+
+
 		}
 
 		ws.on('message', function(message) {
